74a50c473892b76ab3e4829910546f58d7a9509e
PhạmDat1411 <datthaibinh1411@gmail.com>
2024-02-22 15:38:39 +0700

    update

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings. Web;
+using System.Text.Encodings.Web;
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController: Controller
--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d86ed5f5ea6a510f81c279ef261cdecc5ee6cf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85bba59a97c55feea8ece4ad3eb2e1ebd7d30078")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
